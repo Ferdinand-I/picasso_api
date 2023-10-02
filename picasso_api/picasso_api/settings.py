455e490f79b81af4094c328b06904bcb183d09a5
Антон Борисов <ivan-kruzenstern@yandex.ru>
2023-10-03 01:59:22 +0300

    Changed broker hostname.

--- picasso_api/picasso_api/settings.py
+++ picasso_api/picasso_api/settings.py
@@ -147,6 +147,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
+CELERY_BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
 CELERY_ACCEPT_CONTENT = ['pickle', ]
 CELERY_TASK_SERIALIZER = 'pickle'
